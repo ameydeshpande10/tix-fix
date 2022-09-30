a97cd7a654ae1f93d573b984b3d0774e80533d7a
AmeyDeshpande <ameydeshpande94@gmail.com>
2022-09-30 18:22:40 +0530

    minor changes

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-router.post("/movie/add-movie", movieController.AddMovie);
+//Admin-path-movies
 router.post("/movieregister", movieController.AddMovieS);
 router.get("/movie/remove-movie", movieController.DeleteMovie);
+router.post("/addshows", movieController.AddShows);
+
+router.post("/movie/add-movie", movieController.AddMovie);
+router.get("/movie/remove-movie", movieController.DeleteMovie);
+
 router.get("/movie/get-movie/:movie_name", movieController.GetMovie);
 router.get("/movies", movieController.Movies);
 router.get("/movies/cursor", movieController.MoviesCursor);
 router.post("/movie/delete-movie", movieController.DeleteMovie);
 router.get("/moviedetails/:id", movieController.GetMovieDetails);
 
-router.post("/addshows", movieController.AddShows);
 router.post("/showdetails-single/:id", movieController.PostShowDetailsSingle);
 router.get("/showdetails/:id", movieController.GetShowDetails);
 router.post("/bookseats/:id", movieController.PostBookSeat);
